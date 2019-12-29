7c3060e1252214d299eea3e6fa8d25c31a7331e9
KenyDantas <karinykeny@gmail.com>
2019-12-29 09:27:02 -0300

    Inicio configurações de teste

---
+++ src/test/java/com/keny/api/BaseTest.java
@@ -0,0 +1,32 @@
+package com.keny.api;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
+public abstract class BaseTest {
+
+	public static RequestSpecification requestSpecification;
+
+	@BeforeClass
+	public static void setup() {
+		// port was not set because the default is 8080, defined in test.properties
+		requestSpecification = new RequestSpecBuilder()
+				.log(LogDetail.METHOD)
+				.log(LogDetail.URI)
+				.build();
+
+		RestAssured.requestSpecification = requestSpecification;
+	}
+}
