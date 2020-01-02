2e11615b57a0d1616d6644c9c9c4dd74775d7876
KenyDantas <karinykeny@gmail.com>
2020-01-01 21:58:45 -0300

    Tratamento de novas exceções

--- src/main/java/com/keny/api/exception/handler/ResourceExceptionRendler.java
+++ src/main/java/com/keny/api/exception/handler/ResourceExceptionRendler.java
@@ -1,9 +1,12 @@
 package com.keny.api.exception.handler;
 
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -11,12 +14,25 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class ResourceExceptionRendler extends ResponseEntityExceptionHandler {
-	
-	
+
 	@ExceptionHandler({ EmptyResultDataAccessException.class })
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
 			WebRequest request) {
-		String userMessage = "Empty Result Data Access Exception";
+		String userMessage = "Não encontrado o parâmetro de pesquisa";
 		return handleExceptionInternal(ex, userMessage, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+
+	@ExceptionHandler({ ConstraintViolationException.class })
+	public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex,
+			WebRequest request) {
+		String userMessage = "Existe campos obrigatórios não declarados";
+		return handleExceptionInternal(ex, userMessage, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE, request);
+	}
+
+	@Override
+	public ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String userMessage = "Tipo de mídia HTTP não suportada" ;
+		return handleExceptionInternal(ex, userMessage, new HttpHeaders(), HttpStatus.UNSUPPORTED_MEDIA_TYPE, request);
 	}
 }
